b6a7a1ee3b7cab48599e8e92bfa543db4b8a1bfd
jasbjc2004 <160714214+jasbjc2004@users.noreply.github.com>
2024-11-26 16:00:26 +0100

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,7 @@
 from G4Track import *
 import time
+import csv
+import numpy as np
 
 
 def calibration_to_center(sys_id):
@@ -9,28 +11,61 @@ def calibration_to_center(sys_id):
     :param sys_id: system id
     :type sys_id: int
     :return: the sensor who is on the left and right hand
-    :rtype: bool
+    :rtype: int, int, int, bool
     """
-    time.sleep(2)  # wait for the hemisphere to adapt
-    hub_id = get_active_hubs(sys_id, True)[0]
-    station_map = get_station_map(sys_id, hub_id)
+    TRESHOLD = 0.01
+    MAX_ATTEMPTS = 5
+    attempt = 0
 
-    pos0, active_count, data_hubs = None, 0, 0
-    while active_count == 0 & data_hubs == 0:
-        pos0, active_count, data_hubs = get_frame_data(sys_id, [hub_id])
+    while attempt < MAX_ATTEMPTS:
+        frame_reference_orientation_reset(sys_id)
+        frame_reference_translation_reset(sys_id)
 
-    frame_reference_orientation(sys_id, (90, 180, 0))
+        # set_units(sys_id)
+        time.sleep(2)  # wait for the hemisphere to adapt
+        hub_id = get_active_hubs(sys_id, True)[0]
+        station_map = get_station_map(sys_id, hub_id)
 
-    pos_ports = [i for i in range(0, len(station_map)) if station_map[i]]
-    sen1 = pos0.G4_sensor_per_hub[pos_ports[0]]
-    sen2 = pos0.G4_sensor_per_hub[pos_ports[1]]
-    frame_reference_translation(sys_id, ((sen1.pos[0] + sen2.pos[0]) / 2,
-                                         min(sen1.pos[1], sen2.pos[1]),
-                                         min(sen1.pos[2], sen2.pos[2])))
-    if sen1.pos[0] < sen2.pos[0]:
-        lsen, rsen = pos_ports[0], pos_ports[1]
-    else:
-        lsen, rsen = pos_ports[1], pos_ports[0]
+        pos0 = None
+        while pos0 is None:
+            pos0, active_count, data_hubs = get_frame_data(sys_id, [hub_id])
+            if active_count == 0 or data_hubs == 0:
+                time.sleep(0.1)
+                continue
 
-    time.sleep(2)
-    return hub_id, lsen, rsen
+        pos_ports = [i for i in range(0, len(station_map)) if station_map[i]]
+        sen1 = pos0.G4_sensor_per_hub[pos_ports[0]]
+        sen2 = pos0.G4_sensor_per_hub[pos_ports[1]]
+
+        # Find reference using the axis on the source
+        frame_reference_translation(sys_id, (max(sen1.pos[0], sen2.pos[0]),
+                                             (sen1.pos[1] + sen2.pos[1])/2,
+                                             min(sen1.pos[2], sen2.pos[2])))
+
+        frame_reference_orientation(sys_id, (90, 180, 0))
+
+        if sen1.pos[1] < sen2.pos[1]:
+            lsen, rsen = pos_ports[0], pos_ports[1]
+        else:
+            lsen, rsen = pos_ports[1], pos_ports[0]
+
+        time.sleep(2)
+
+        pos0, active_count, data_hubs = None, 0, 0
+        while active_count == 0 & data_hubs == 0:
+            pos0, active_count, data_hubs = get_frame_data(sys_id, [hub_id])
+
+        pos_left = list(pos0.G4_sensor_per_hub[lsen].pos)
+        pos_right = list(pos0.G4_sensor_per_hub[rsen].pos)
+
+        print(abs(abs(pos_left[0]) - pos_right[0]))
+
+        print(f"sensor: {pos_left, pos_right}")
+
+        time.sleep(2)
+        if abs(abs(pos_left[0]) - pos_right[0]) < TRESHOLD:
+            return hub_id, lsen, rsen, True
+
+        attempt += 1
+
+    return None, None, None, False
