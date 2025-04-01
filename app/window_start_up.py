e8518a4f169e245efa40c2c0f1456ba9794c2b9d
jasbjc2004 <160714214+jasbjc2004@users.noreply.github.com>
2025-04-01 20:48:19 +0200

    Added file reopening

--- app/window_start_up.py
+++ app/window_start_up.py
@@ -1,5 +1,7 @@
+import os
+
 from PySide6.QtWidgets import (
-    QVBoxLayout, QWidget, QPushButton
+    QVBoxLayout, QWidget, QPushButton, QFileDialog, QMessageBox
 )
 
 from window_set_up import SetUp
@@ -15,6 +17,7 @@ class StartUp(QWidget):
         self.button_new = QPushButton("New Project")
         self.button_new.clicked.connect(self.open_setup)
         self.button_old = QPushButton("Open existing Project")
+        self.button_old.clicked.connect(self.reopen_setup)
 
         layout.addWidget(self.button_new)
         layout.addWidget(self.button_old)
@@ -26,3 +29,39 @@ class StartUp(QWidget):
         self.setup = SetUp()
         self.setup.show()
         self.close()
+
+    def reopen_setup(self):
+        folder = QFileDialog.getExistingDirectory(self, "Select Trial Directory", os.path.expanduser("~/Documents"))
+        if folder:
+            self.setup = SetUp(folder)
+            self.setup.show()
+            self.close()
+
+
+        """import tkinter as tk
+from tkinter import filedialog, Tk
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import butter, filtfilt
+
+# Function to select file
+def select_file():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx")])
+    return file_path
+
+# Select file
+file_path = select_file()
+
+if file_path:
+    # Load Excel data
+    df = pd.read_excel(file_path)
+
+    # Extract time and x data 
+    t = df.iloc[:, 0].values
+    f = df.iloc[:, 1].values 
+   
+else:
+    print("No file selected.")"""
