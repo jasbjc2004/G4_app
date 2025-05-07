0bc8f49be93fc6706e153d4a7feaf52083cbcc31
jasbjc2004 <160714214+jasbjc2004@users.noreply.github.com>
2025-05-07 10:15:29 +0200

    Update data_processing.py

--- app/data_processing.py
+++ app/data_processing.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import argrelextrema
 
 from constants import MAX_HEIGHT_NEEDED, POSITION_BUTTON, MAX_LENGTH_NEEDED, MIN_HEIGHT_NEEDED, MIN_LENGTH_NEEDED, \
     THRESHOLD_BOTH_HANDS, SPEED_THRESHOLD, HEIGHT_BOX, THRESHOLD_CHANGED_HANDS_MEAS, fs
@@ -158,66 +159,90 @@ def calculate_events(pos_left, pos_right, case, score):
 
     e6 = len(pos_left) - 1
 
-    v_th = np.array([pos[3] for pos in trigger_hand])
-    v_bh = np.array([pos[3] for pos in box_hand])
+    try:
+        v_th = np.array([pos[3] for pos in trigger_hand])
+        v_bh = np.array([pos[3] for pos in box_hand])
 
-    a_bh = np.array(np.diff(v_bh) * fs)
+        a_bh = np.array(np.diff(v_bh) * fs)
 
-    # calculating e1
-    e1 = np.argmax(v_bh > 0.05)
-    while e1 > 1 and a_bh[e1] >= 0:
-        e1 -= 1
-    print("e1", e1)
+        # calculating e1
+        e1 = np.argmax(v_bh > SPEED_THRESHOLD)
+        while e1 > 1 and a_bh[e1] >= 0:
+            e1 -= 1
+        print("e1", e1)
 
-    if score != 3:
-        return e1, 0, 0, 0, 0
+        if score != 3:
+            return e1, 0, 0, 0, 0
 
-    # calculating e2
-    piek_1 = np.argmax(v_bh[e1:e1 + 51]) + e1 - 1
-    print("piek_1 ", piek_1)
-    piek_2 = np.argmax(v_bh[piek_1 + 51:piek_1 + 1001]) + piek_1 + 50 - 1
-    print("piek_2 ", piek_2)
-    e2 = np.argmin(v_bh[piek_1:piek_2]) + piek_1 - 1
-    print("e2 ", e2)
+        # calculating e2
+        piek_1 = np.argmax(v_bh[e1:e1 + 51]) + e1 - 1
+        print("piek_1 ", piek_1)
+        piek_2 = np.argmax(v_bh[piek_1 + 51:piek_1 + 1001]) + piek_1 + 50 - 1
+        print("piek_2 ", piek_2)
+        e2 = np.argmin(v_bh[piek_1:piek_2]) + piek_1 - 1
+        print("e2 ", e2)
 
-    # calculating e3
-    z_bh = np.array([pos[2] for pos in box_hand])
-    e3 = np.argmax(z_bh[1:e6])
-    print("e3", e3)
+        # calculating e3
+        z_bh = np.array([pos[2] for pos in box_hand])
+        e3 = np.argmax(z_bh[1:e6])
+        print("e3", e3)
 
-    # calculating e4 and e5
-    start_trigger = pos_left[0]
+        # calculating e4 and e5
+        start_trigger = pos_left[0]
 
-    e4 = 0
-    for i in range(len(pos_left)):
-        if trigger_hand[i][2] < MAX_HEIGHT_NEEDED + start_trigger[2] and \
-                trigger_hand[i][1] < MAX_LENGTH_NEEDED + start_trigger[1]:
-            e4 = i
+        e4 = 0
+        for i in range(len(pos_left)):
+            if trigger_hand[i][2] < MAX_HEIGHT_NEEDED + start_trigger[2] and \
+                    trigger_hand[i][1] < MAX_LENGTH_NEEDED + start_trigger[1]:
+                e4 = i
 
-    while v_th[e4] >= SPEED_THRESHOLD:
-        e4 -= 1
+        while e4 > 1 and v_th[e4-1] >= SPEED_THRESHOLD:
+            e4 -= 1
 
-    e5 = len(pos_left) - 1
-    while v_th[e5] >= SPEED_THRESHOLD:
-        e5 -= 1
+        e5 = len(pos_left) - 1
+        while e5 > 1 and v_th[e5-1] >= SPEED_THRESHOLD:
+            e5 -= 1
 
-    if abs(e5 - e1) < 60:
-        e5 = e4
+        if abs(e5 - e1) < 60:
+            e5 = e4
 
-    return e1, e2, e3, e4, e5
+        return e1, e2, e3, e4, e5
+    except:
+        return 0,0,0,0,0
 
 
 def calculate_e6(xs):
     return len(xs) - 1
 
 
-def calculate_extra_info(events):
+def calculate_extra_parameters(events, trigger_hand, box_hand):
     e1, e2, e3, e4, e5, e6 = events
+    bx = np.array([pos[0] for pos in box_hand])
+    by = np.array([pos[1] for pos in box_hand])
+    bz = np.array([pos[2] for pos in box_hand])
+    bv = np.array([pos[3] for pos in box_hand])
+
+    tx = np.array([pos[0] for pos in trigger_hand])
+    ty = np.array([pos[1] for pos in trigger_hand])
+    tz = np.array([pos[2] for pos in trigger_hand])
+    tv = np.array([pos[3] for pos in trigger_hand])
+
+    # we berekenen 4 bimanuele parameters en 10 unimanuele parameters
 
     # total time = trigger press - start first movement
     # e5 kan hier gelijk zijn aan e5 dus zoek dat uit
     tt = (e6 - min(e1, e4)) / fs
 
+    # temp coupling = start trigger hand - start second phase of box opening hand ??
+    temp_coupling = (e4 - e2) / fs
+
+    # movement overlap = end lid opening - start trigger hand
+    mov_overlap = ((e3 - e4) / fs)
+
+    # goal synchronization = trigger press - end lid opening
+    goal_sync = (e6 - e3) / fs
+
+    # unimanuele parameters
     # time box hand = end lid opening - start box hand
     t_bh = (e3 - e1) / fs
 
@@ -229,15 +254,71 @@ def calculate_extra_info(events):
 
     # time trigger hand = trigger press - start trigger hand
     # rekening houden met e4 en e5
-    t_th = (e6 - e5) / fs
+    t_th = (e6 - e4) / fs
 
-    # temp coupling = end box hand - start trigger hand
-    temp_coupling = (e3 - e5) / fs
+    # if start / endpoint is also a max => not taken into account. if need => first local max of b then sum in range e1: e3
+    subset = bv[e1:e3]
 
-    # movement overlap = end lid opening - start trigger hand
-    mov_overlap = ((e3 - e5) / fs) / tt * 100
+    max_bh = argrelextrema(subset, np.greater)[0]
+    min_bh = argrelextrema(subset, np.less)[0]
 
-    # goal synchronization = trigger press - end lid opening
-    goal_sync = (e6 - e3) / fs
+    # Count the number of local extrema
+    smooth_bh = len(max_bh) + len(min_bh)
 
-    return tt, t_bh, t_bh_p1, t_bh_p2, t_th, temp_coupling, mov_overlap, goal_sync
+    subset_t = tv[e1:e3]
+
+    max_th = argrelextrema(subset_t, np.greater)[0]
+    min_th = argrelextrema(subset_t, np.less)[0]
+
+    # Count the number of local extrema
+    smooth_th = len(max_th) + len(min_th)
+
+    # path-length of box hand (d=distance)
+    dbx = bx[e1:e3]
+    dby = by[e1:e3]
+    dbz = bz[e1:e3]
+
+    dbx = np.diff(dbx)
+    dby = np.diff(dby)
+    dbz = np.diff(dbz)
+
+    d_bh = np.sqrt((dbx ** 2) + (dby ** 2) + (dbz ** 2))
+    d_bh = np.sum(d_bh)
+
+    # path-length of box hand - eerste fase tot aan de doos
+    dbx_p1 = bx[e1:e2]
+    dby_p1 = by[e1:e2]
+    dbz_p1 = bz[e1:e2]
+
+    dbx_p1 = np.diff(dbx_p1)
+    dby_p1 = np.diff(dby_p1)
+    dbz_p1 = np.diff(dbz_p1)
+
+    d_bh_p1 = np.sqrt((dbx_p1 ** 2) + (dby_p1 ** 2) + (dbz_p1 ** 2))
+    d_bh_p1 = np.sum(d_bh_p1)
+
+    # path-length of box hand - tweede fase deksel van de doos op hoogste punt
+    dbx_p2 = bx[e2:e3]
+    dby_p2 = by[e2:e3]
+    dbz_p2 = bz[e2:e3]
+
+    dbx_p2 = np.diff(dbx_p2)
+    dby_p2 = np.diff(dby_p2)
+    dbz_p2 = np.diff(dbz_p2)
+
+    d_bh_p2 = np.sqrt((dbx_p2 ** 2) + (dby_p2 ** 2) + (dbz_p2 ** 2))
+    d_bh_p2 = sum(d_bh_p2)
+
+    # path-length of trigger hand of triggerhand, not yet activated as e5 still needs to be calculated ??? -> check na wat dit betekent
+    dtx = tx[e4:e6]
+    dty = ty[e4:e6]
+    dtz = tz[e4:e6]
+
+    dtx = np.diff(dtx)
+    dty = np.diff(dty)
+    dtz = np.diff(dtz)
+    d_th = np.sqrt((dtx ** 2) + (dty ** 2) + (dtz ** 2))
+    d_th = np.sum(d_th)
+
+    return [tt, temp_coupling, mov_overlap, goal_sync], \
+        [t_bh, t_bh_p1, t_bh_p2, t_th, smooth_bh, smooth_th, d_bh, d_bh_p1, d_bh_p2, d_th]
