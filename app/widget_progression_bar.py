0a144302e311a6d6dc626e2520213aeb12a25ba1
jasbjc2004 <160714214+jasbjc2004@users.noreply.github.com>
2025-05-14 14:19:13 +0200

    Added info to widget_progression_bar.py

--- app/widget_progression_bar.py
+++ app/widget_progression_bar.py
@@ -1,5 +1,6 @@
 import os
 
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import (
     QVBoxLayout, QLabel, QProgressBar, QDialog
 )
@@ -7,9 +8,14 @@ from PySide6.QtCore import Qt, QTimer
 
 
 class ProgressionBar(QDialog):
+    """
+    Show a progress bar when downloading a file
+    """
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle("Help")
+        file_directory = (os.path.dirname(os.path.abspath(__file__)))
+        dir_icon = os.path.join(file_directory, 'NEEDED/PICTURES/hands.ico')
+        self.setWindowIcon(QIcon(dir_icon))
         self.setGeometry(550, 275, 350, 75)
 
         self.setWindowFlags(Qt.Window | Qt.WindowStaysOnTopHint)
@@ -29,6 +35,10 @@ class ProgressionBar(QDialog):
         self.setLayout(layout)
 
     def set_progress(self, value):
+        """
+        Set the progress displayed on the bar
+        :param value: value to set the progress bar to
+        """
         if value < 100:
             self.progress_bar.setValue(int(round(value)))
         else:
