01e18d9edd02ce51913781ec172ddad3b9be2ff5
jasbjc2004 <160714214+jasbjc2004@users.noreply.github.com>
2025-07-09 15:48:53 +0200

    Added settings

--- app/constants.py
+++ app/constants.py
@@ -1,31 +1,44 @@
-MAX_TRAILS = 21
+#MAX_TRAILS = 21
 
-READ_SAMPLE = False
+READ_SAMPLE = True
 BEAUTY_SPEED = True
-SERIAL_BUTTON = True
+#SERIAL_BUTTON = True
 
-MAX_ATTEMPTS = 10
+#MAX_ATTEMPTS_CONNECT = 10
 
-MAX_HEIGHT_NEEDED = 2  # cm
-MAX_LENGTH_NEEDED = 2
-MIN_HEIGHT_NEEDED = 3 # cm
-MIN_LENGTH_NEEDED = 3
-POSITION_BUTTON = (0,15,1.5)
+#MAX_HEIGHT_NEEDED = 2  # cm
+#MAX_LENGTH_NEEDED = 2
+#MIN_HEIGHT_NEEDED = 3 # cm
+#MIN_LENGTH_NEEDED = 3
+#POSITION_BUTTON = (0,15,1.5)
 
-fs = 120
-fc = 10
+#fs = 120
+#fc = 10
 
-ORDER_FILTER = 2  # 4
-SPEED_FILTER = True
+#ORDER_FILTER = 2  # 4
+#SPEED_FILTER = True
 
-SENSORS_USED = 2
+#SENSORS_USED = 2
 
-THRESHOLD_BOTH_HANDS = 20
-THRESHOLD_CHANGED_HANDS_MEAS = 20
-SPEED_THRESHOLD = 0.05
+#THRESHOLD_BOTH_HANDS = 20
+#THRESHOLD_CHANGED_HANDS_MEAS = 20
+#SPEED_THRESHOLD = 0.05
 
-HEIGHT_BOX = 15
+#HEIGHT_BOX = 15
 
-NUMBER_EVENTS = 6
-COLORS_EVENT = ['blue', 'purple', 'orange', 'yellow', 'pink', 'black']
-LABEL_EVENT = ['e1', 'e2', 'e3', 'e4', 'e5', 'e6']
+#NUMBER_EVENTS = 6
+#COLORS_EVENT = ['blue', 'purple', 'orange', 'yellow', 'pink', 'black']
+#LABEL_EVENT = ['e1', 'e2', 'e3', 'e4', 'e5', 'e6']
+
+COLORS = [
+    ("#FF0000", "Red"),  # Pure red
+    ("#00FF00", "Green"),  # Pure green
+    ("#0000FF", "Blue"),  # Pure blue
+    ("#FFFF00", "Yellow"),  # Pure yellow
+    ("#FF00FF", "Magenta"),  # Pure magenta
+    ("#00FFFF", "Cyan"),  # Pure cyan
+    ("#FF8000", "Orange"),  # Orange
+    ("#9932CC", "Purple"),  # Purple
+    ("#000000", "Black"),  # For contrast on light backgrounds
+    ("#FF0080", "Pink")  # Pink - distinct from red/magenta
+]
